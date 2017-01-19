25cc58de49c61451562e16deb4396718474d0934
Yangchen Shen <shyangch@gmail.com>
2017-01-19 15:57:37 -0600

    studio0

--- coursesupport/timing/examples/Quadratic.java
+++ coursesupport/timing/examples/Quadratic.java
@@ -27,10 +27,17 @@ public class Quadratic extends QuietAlgorithm {
 	@Override
 	public void run() {
 		for (int i=0; i < n; ++i) {
+			//ticker1
+			//ticker.tick();
 			for (int j=0; j < n; ++j) {
 				//
 				// Statement below takes one operation
 				this.value = this.value + i;
+				ticker.tick();
+				//ticker2
+				ticker.tick();
+				ticker.tick();
+				ticker.tick();
 				ticker.tick();
 			}
 		}	
