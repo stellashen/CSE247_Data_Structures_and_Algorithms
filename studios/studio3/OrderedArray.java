9785ca8463ffcbfd935dfa6054b93b3aa85a8d48
Yangchen Shen <shyangch@gmail.com>
2017-02-09 16:02:16 -0600

    studio3

--- studios/studio3/OrderedArray.java
+++ studios/studio3/OrderedArray.java
@@ -4,19 +4,19 @@ public class OrderedArray<T extends Comparable<T>> implements PriorityQueue<T> {
 
 	public T[] array;
 	private int size;
-	
+
 	@SuppressWarnings("unchecked")
 	public OrderedArray(int maxSize) {
 		array = (T[]) new Comparable[maxSize];
 		size = 0;
 	}
-	
+
 	@Override
 	public boolean isEmpty() {
 		//
 		// FIXME
 		//
-		return false;
+		return (size==0);
 	}
 
 	@Override
@@ -24,14 +24,54 @@ public class OrderedArray<T extends Comparable<T>> implements PriorityQueue<T> {
 		//
 		// FIXME
 		//
+		if(size==0){
+			array[0]=thing;
+			size = 1;
+		}
+
+		else{
+			int i=0;
+			while(thing.compareTo(array[i])>=0 && i<size){
+				i++;
+			}
+
+			if(i==size-1){
+				array[size]=thing;
+				size = size+1;
+			}
+			else{
+				size = size + 1;
+				//move all the elements on the right side to the next position
+				for(int j = size-1; j > i;j--){
+					array[j]=array[j-1];
+				}
+
+				array[i]=thing;
+			}
+		}
 	}
-	
+
 	@Override
 	public T extractMin() {
 		//
 		// FIXME
 		//
-		return null;
+		if(size==0){
+			return null;
+		}
+		else if(size==1){
+			T head = array[0];
+			size = 0;
+			return head;
+		}
+		else{
+			T head = array[0]; 
+			for(int i=0;i<size;i++){
+				array[i]=array[i+1];
+			}
+			size = size - 1;
+			return head;
+		}
 	}
 
 }
--- studios/studio3/UnorderedList.java
+++ studios/studio3/UnorderedList.java
@@ -16,7 +16,7 @@ public class UnorderedList<T extends Comparable<T>> implements PriorityQueue<T> 
 		//
 		// FIXME
 		//
-		return false;
+		return list.isEmpty();
 	}
 
 	@Override
@@ -24,6 +24,7 @@ public class UnorderedList<T extends Comparable<T>> implements PriorityQueue<T> 
 		//
 		// FIXME
 		//
+		list.add(thing);
 	}
 
 	@Override
@@ -31,7 +32,24 @@ public class UnorderedList<T extends Comparable<T>> implements PriorityQueue<T> 
 		//
 		// FIXME
 		//
-		return null;
+		if(list.size()==0){
+			return null;
+		}
+		else if(list.size()==1){
+//			T firstElement = list.get(0);
+//			list.remove(0);
+//			return firstElement;
+			return list.remove(0);
+		}
+		else{
+			int minPos = 0;
+			for (int i=0; i < list.size(); i++){
+				if (list.get(i).compareTo(list.get(minPos)) < 0){
+					minPos = i;
+				}
+			}
+			return list.remove(minPos);
+		}
 	}
 
 }
