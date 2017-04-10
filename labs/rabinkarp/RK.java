39a815fa34a3ec4c2449157e12828f882b577f04
Yangchen Shen <shyangch@gmail.com>
2017-04-10 16:24:08 -0500

    lab9 draft

--- labs/rabinkarp/RK.java
+++ labs/rabinkarp/RK.java
@@ -1,30 +1,40 @@
 package rabinkarp;
 
 public class RK {
-	
 	//
 	// Be sure to look at the write up for this assignment
 	//  so that you get full credit by satisfying all
 	//  of its requirements
 	//
-	
+
+	private int m;
+	private int[] c;
+	private int h;
+	private int i;
 
 	/**
 	 * Rabin-Karp string matching for a window of the specified size
 	 * @param m size of the window
 	 */
 	public RK(int m) {
-
+		this.m = m;
+		c = new int[m];
+		h = 0;
+		i = 0;
 	}
-	
-
 	/**
 	 * Compute the rolling hash for the previous m-1 characters with d appended.
 	 * @param d the next character in the target string
 	 * @return
 	 */
 	public int nextCh(char d) {
-		return 0;
+		// int max value is 2,147,483,647
+		// 31^7 will cause overflow
+		int p=(int)Math.pow(31, m);
+		h = (h*31 - c[i%m]*p % 511 + d) % 511;
+		c[i%m]=d;
+		i++;
+		return h;
 	}
 
 }
